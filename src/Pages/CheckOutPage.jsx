0f5e664b29e328d493387c342b49b1420d01feaf
Jyotishmoy12 <93263133+Jyotishmoy12@users.noreply.github.com>
2025-03-27 14:59:13 +0530

    okay

--- src/Pages/CheckOutPage.jsx
+++ src/Pages/CheckOutPage.jsx
@@ -323,7 +323,7 @@ const CheckoutPage = () => {
                             </p>
                           </div>
                         </div>
-                        <p className="font-bold">₹₹{(Number(item.price) * item.quantity).toFixed(2)}
+                        <p className="font-bold">₹{(Number(item.price) * item.quantity).toFixed(2)}
                         </p>
                       </div>
                     ))}
